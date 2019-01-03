104f5866dda709aa9d428374819a474206d30495
Bix2 <bibi.treffers@outlook.com>
2019-01-03 06:11:11 +0100

    build admin form

--- src/Form/adminForm.php
+++ src/Form/adminForm.php
@@ -1,10 +1,38 @@
 <?php
 
-namespace  Drupal\thomas_more_sweets\Form;
+namespace  Drupal\icecream\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
-class SettingsForm extends FormBase {
+class adminForm extends FormBase {
+    public function getFormId() {
+        return 'admin_icecream_form';
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state) {
+        $form['min_order_waffle'] = [
+            '#type' => 'number',
+            '#title' => 'Minimum order for waffles',
+            '#default_value' => \Drupal::state()->get('min_order_waffle')
+        ];
+
+        $form['min_order_icecream'] = [
+            '#type' => 'number',
+            '#title' => 'Minimum order for icecream',
+            '#default_value' => \Drupal::state()->get('min_order_icecream')
+        ];
+              
+              
+        $form['submit'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('Submit'),
+        ];
     
+        return $form;
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+        
+    }
 }
